e1209f7aa5c7efff004cfcfda583ea38c1f7189e
rvdpas <rvdpas94@gmail.com>
2017-03-31 14:03:16 +0200

    fix styles header

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config()
 var apiUrl = process.env.API_URL;
 var apiKey = process.env.API_KEY;
 var searchKey = process.env.API_SEARCH_KEY;
+var searchKeyExtended = process.env.API_SEARCH_EXTENDED;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +27,19 @@ app.use(function(req, res, next) {
 	res.locals.errors = null;
 	next();
 });
+
+app.post('/result', search);
+function search(req, res){
+  var query = req.body.input;
+  request(apiUrl + apiKey + searchKey + query + searchKeyExtended, function (error, response, body) {
+      if(!error && response.statusCode === 200) {
+          data = JSON.parse(body);
+          res.render('index.ejs', {
+              residences: data
+          });
+      }
+  });
+}
 
 // Overview Page
 app.get('/', function (req, res) {
